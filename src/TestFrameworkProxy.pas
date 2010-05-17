5afa6714d93b2846719f4d8a36fb51927dc1c16a
Paul Spain <pvspain@gmail.com>
2010-05-17 04:05:52 +0000

    tweaks
    git-svn-id: svn://svn.code.sf.net/p/dunit2/code/trunk@60 9728aa66-fbd7-4c87-b7c8-c4b37a56c05d

--- src/TestFrameworkProxy.pas
+++ src/TestFrameworkProxy.pas
@@ -464,7 +464,8 @@ end;
 procedure TITestResult.ReleaseListeners;
 begin
   try
-    FTestListenerProxy.ReleaseListeners;
+    if Assigned(FTestListenerProxy) then
+      FTestListenerProxy.ReleaseListeners;
   finally
     FTestListenerProxy := nil;
   end;
